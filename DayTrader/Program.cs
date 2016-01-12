1fd69055284520b6e887afd5b169cc925f1f93f3
Lee Beenen <l.beenen@diract.nl>
2016-01-12 17:38:40 +0100

    Added check to see if the lowest index isn't in danger of being the last or second to last one.

--- DayTrader/Program.cs
+++ DayTrader/Program.cs
@@ -4,27 +4,33 @@ using System.Linq;
 
 namespace DayTrader
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
       var input = args[0];
 
-      Console.WriteLine("The best trades are {0}", 
+      Console.WriteLine("The best trades are {0}",
         DayTrade(
           input.Split(' ').Select(value => float.Parse(value, CultureInfo.InvariantCulture)).ToArray()
-        )
-      );
+          )
+        );
 
       Console.ReadLine();
     }
-
 
     private static String DayTrade(float[] convertedInput)
     {
       var lowest = convertedInput.Min();
 
-      var highestAfterLowest = convertedInput.Skip(Array.IndexOf(convertedInput, lowest) + 2).Max(); // + 1 to begin searching after the lowest value, +1 to take into account it can never be the next value.
+      var lowestIndex = Array.IndexOf(convertedInput, lowest);
+
+      if (lowestIndex >= convertedInput.Length - 2)
+        //If the index of the smallest value within the sequence is either the second to last or the last value, there can never be a bigger value afterwards, so return the lowest.
+        return String.Format(CultureInfo.InvariantCulture, "{0}, no highest was found.", lowest);
+
+      var highestAfterLowest = convertedInput.Skip(Array.IndexOf(convertedInput, lowest) + 2).Max();
+      // + 1 to begin searching after the lowest value, +1 to take into account it can never be the next value.
 
       return String.Format(CultureInfo.InvariantCulture, "{0} {1}", lowest, highestAfterLowest);
     }
